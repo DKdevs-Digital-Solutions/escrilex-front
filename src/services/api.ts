8fdce3e533af8db101e6b5475c2738f07c145b12
Guilvitor17 <guilvitor17@gmail.com>
2026-05-04 09:25:32 -0300

    subindo att

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/",
-  // baseURL: import.meta.env.VITE_API_URL,
+  // baseURL: "/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
